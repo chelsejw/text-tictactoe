aedd3a0346db724b663496826ce64d6e3326ab43
chelsejw <chelsejw@gmail.com>
2020-09-03 22:07:24 +0800

    add win check, update prompt config to exit on ^C

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')()
+const prompt = require('prompt-sync')({sigint: true}) 
 
 const getSquarePosition = (squareNum, boardSize) => {
   let row = Math.ceil(squareNum / boardSize) - 1;
@@ -11,15 +11,17 @@ const getSquarePosition = (squareNum, boardSize) => {
 };
 
 const getValueAtSquare = (squareNum, boardSize, gameBoard) => {
-  let pos = getSquarePosition(squareNum, boardSize);
+  let {row, col} = getSquarePosition(squareNum, boardSize);
   //First num of row would be at col 0
-  return gameBoard[pos.row][pos.col]
+  return gameBoard[row][col]
 };
 
 const markSquare = (squareNum, boardSize, gameBoard, marker) => {
-    const {row, col} = getSquarePosition(squareNum, boardSize)
 
-    if (!gameBoard[row][col]) {
+    if (squareNum<=0 || squareNum > boardSize*boardSize) return false
+
+    const {row, col} = getSquarePosition(squareNum, boardSize)
+    if (gameBoard[row][col]===null) {
         gameBoard[row][col] = marker
     } else {
         return false //If it has already been marked
@@ -80,15 +82,117 @@ while (isNaN(boardSize) || boardSize < 3) {
   );
 }
 
+
+
 const gameBoard = initialiseGameBoard(boardSize)
 displayBoard(gameBoard, boardSize);
 
 let turnNum = 0
 let gameIsDone = false
+let thereIsWinner = false
 
+// O(1) time complexity
+const checkWin = (num) => {
+  const { col, row } = getSquarePosition(num, boardSize);
+  const latestSquare = gameBoard[row][col];
+  //Horizontal:
+  // If col-2 >= 0 then need to check for front 2
+  // If col+2 < boardSize then check for back 2.
+  if (col - 1 >= 0 && col + 1 < boardSize) {
+    //If square has at least one space to the left and right
+    if (gameBoard[row][col - 1] === latestSquare && latestSquare === gameBoard[row][col + 1])
+      return true;
+  }
+
+  if (col - 2 >= 0) {
+    //If the square has at least two spaces to the left
+    if (
+      gameBoard[row][col - 2] === latestSquare &&
+      gameBoard[row][col - 1] === latestSquare
+    ) {
+      return true;
+    }
+  }
+  if (col + 2 < boardSize) {
+    //If the square has at least two spaces to the right
+    if (latestSquare === gameBoard[row][col + 1] && latestSquare === gameBoard[row][col + 2])
+      return true;
+  }
+  //VERTICAL:
+  if (row - 1 >= 0 && row + 1 < boardSize) {
+    if (gameBoard[row - 1][col] === latestSquare && latestSquare === gameBoard[row + 1][col])
+      return true;
+  }
+  if (row - 2 >= 0) {
+    if (gameBoard[row - 2][col] === latestSquare && gameBoard[row - 1][col] === latestSquare)
+      return true;
+  }
+  if (row + 2 < boardSize) {
+    if (latestSquare === gameBoard[row + 1][col] && latestSquare === gameBoard[row + 2][col])
+      return true;
+  }
+  // DIAGONAL:
+  if (row - 2 >= 0 && col + 2 < boardSize) {
+    //If there are two squares diagonally upper right
+    if (
+      latestSquare === gameBoard[row - 1][col + 1] && latestSquare ===
+      gameBoard[row - 2][col + 2]
+    )
+      return true;
+  }
+  if (row + 2 < boardSize && col - 2 >= 0) {
+    // If there are two squares diagonally lower left
+    if (
+      latestSquare === gameBoard[row + 1][col - 1] && latestSquare ===
+      gameBoard[row + 2][col - 2]
+    )
+      return true;
+  }
+  if (
+    row + 1 < boardSize &&
+    row - 1 >= 0 &&
+    col + 1 < boardSize &&
+    col - 1 >= 0
+  ) {
+    //If there is one space diagonally lower left and upperright
+    if (
+      gameBoard[row + 1][col - 1] === latestSquare && latestSquare ===
+      gameBoard[row - 1][col + 1]
+    )
+      return true;
+  }
+  if (row - 2 >= 0 && col - 2 >= 0) {
+    //If there are two squares diagonally upper left
+    if (
+      gameBoard[row - 2][col - 2] === latestSquare && gameBoard[row - 1][col - 1] ===
+      latestSquare
+    )
+      return true;
+  }
+  if (row + 2 < boardSize && col + 2 < boardSize) {
+    //If there are two squares diagonally lower right
+    if (
+      latestSquare === gameBoard[row + 1][col + 1] && latestSquare ===
+      gameBoard[row + 2][col + 2]
+    )
+      return true;
+  }
+  if (
+    row - 1 >= 0 &&
+    col - 1 >= 0 &&
+    row + 1 < boardSize &&
+    col + 1 < boardSize
+  ) {
+    if (
+      gameBoard[row - 1][col - 1] === latestSquare && latestSquare === 
+      gameBoard[row + 1][col + 1]
+    )
+      return true;
+  }
+  return false;
+};
 const makeMove = (turnNum) => {
 
-    console.log(`Turn num is ${turnNum}`)
     // If turn number is even, it is player 1. else, it's player 2
     const currentPlayer = turnNum%2===0 ? player1 : player2
     const marker = currentPlayer==player1 ? "X" : "O"
@@ -106,12 +210,20 @@ const makeMove = (turnNum) => {
     }
     displayBoard(gameBoard, boardSize)
     turnNum++;
+
+    if (turnNum >4 && checkWin(targetSquare)) { //Because there can only be a winner on the fifth move.
+        gameIsDone = true
+        thereIsWinner = true
+    }
+
     if (turnNum === boardSize * boardSize) {
         gameIsDone = true;
     }
 
-    if (gameIsDone) {
-        return console.log("Game is over!")
+    if (gameIsDone && !thereIsWinner) {
+        return console.log("Game is over! There was no winner.")
+    } else if (gameIsDone && thereIsWinner) {
+        return console.log(`Game is over! ${currentPlayer} won!`)
     }
 
     return makeMove(turnNum)
