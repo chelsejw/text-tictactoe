b87f15a21c14fc4e73515e2519f5b37f6676c7e1
chelsejw <chelsejw@gmail.com>
2020-09-03 12:01:01 +0800

    players can mark squares, game is considered done when all squares are filled

--- game.js
+++ game.js
@@ -1,17 +1,36 @@
 const prompt = require('prompt-sync')()
 
-const getValueAtIndex = (num, boardSize, gameBoard) => {
-  let row = Math.ceil(num / boardSize) - 1;
+const getSquarePosition = (squareNum, boardSize) => {
+  let row = Math.ceil(squareNum / boardSize) - 1;
   let firstNumOfRow = boardSize * row + 1;
-  let col = num - firstNumOfRow;
-  //First num of row would be at col 0
-  return gameBoard[row][col]
+  let col = squareNum - firstNumOfRow;
+  return {
+    row,
+    col,
+  };
 };
+
+const getValueAtSquare = (squareNum, boardSize, gameBoard) => {
+  let pos = getSquarePosition(squareNum, boardSize);
+  //First num of row would be at col 0
+  return gameBoard[pos.row][pos.col]
+};
+
+const markSquare = (squareNum, boardSize, gameBoard, marker) => {
+    const {row, col} = getSquarePosition(squareNum, boardSize)
+
+    if (!gameBoard[row][col]) {
+        gameBoard[row][col] = marker
+    } else {
+        return false //If it has already been marked
+    }
+    return true
+}
 
 const displayBoard = (gameBoard, boardSize) => {
   let row = "";
   for (let i = 1; i <= boardSize * boardSize; i++) {
-    let marker = getValueAtIndex(i, boardSize, gameBoard)
+    let marker = getValueAtSquare(i, boardSize, gameBoard)
     //If the value is null, then use the index instead
     if (!marker) marker = i
 
@@ -47,7 +66,7 @@ const initialiseGameBoard = (boardSize) => {
 // const player2 = prompt("Enter Player 2 Name: ");
 
 
-// GLOBAL VARiABLES: player1, player2, boardSize, gameBoard, turnNum
+// GLOBAL VARiABLES: player1, player2, boardSize, gameBoard, turnNum, gameIsDone
 const player1 = "Chelsea";
 const player2 = "Misty";
 
@@ -55,7 +74,7 @@ let boardSize = prompt(
   "Enter board size as an integer greater than or equal to 3."
 );
 
-while (isNaN(boardSize)) {
+while (isNaN(boardSize) || boardSize < 3) {
   boardSize = prompt(
     "Invalid board size. Enter board size as an integer greater than or equal to 3."
   );
@@ -64,22 +83,38 @@ while (isNaN(boardSize)) {
 const gameBoard = initialiseGameBoard(boardSize)
 displayBoard(gameBoard, boardSize);
 
-const turnNum = 0
+let turnNum = 0
+let gameIsDone = false
 
 const makeMove = (turnNum) => {
+
+    console.log(`Turn num is ${turnNum}`)
     // If turn number is even, it is player 1. else, it's player 2
     const currentPlayer = turnNum%2===0 ? player1 : player2
     const marker = currentPlayer==player1 ? "X" : "O"
     console.log(`It is ${currentPlayer}'s turn!`)
-    const targetIndex = prompt(
+    let targetSquare = prompt(
       ` Please enter a number for where you want to make your move.`
     );
 
-    //Get the current value
-    let valueAtIndex = getValueAtIndex(targetIndex);
-
-    
-
+    let madeAValidMove = markSquare(targetSquare, boardSize, gameBoard, marker)
+    while (!madeAValidMove) {
+      targetSquare = prompt(
+        "Sorry, that was an invalid move. Please enter a number for a square that has not already been marked."
+      );
+      madeAValidMove = markSquare(targetSquare, boardSize, gameBoard, marker);
+    }
+    displayBoard(gameBoard, boardSize)
     turnNum++;
-    return displayBoard(gameBoard, boardSize)
+    if (turnNum === boardSize * boardSize) {
+        gameIsDone = true;
+    }
+
+    if (gameIsDone) {
+        return console.log("Game is over!")
+    }
+
+    return makeMove(turnNum)
 }
+
+makeMove(turnNum)
